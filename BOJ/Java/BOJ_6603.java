68f97ea22a4b30b41323075269dbb452029d8a84
SooYeonKim <lovesu5632@gmail.com>
2021-12-21 23:22:53 +0900

    Add: 2021-12-21

---
+++ BOJ/Java/BOJ_6603.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_6603 {
+	
+	static int k;
+	static int[] array;
+	static int[] selected;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		while (true) {
+			st = new StringTokenizer(br.readLine(), " ");
+			k = Integer.parseInt(st.nextToken());
+			if (k == 0) break;
+			
+			array = new int[k];
+			selected = new int[6];
+			for (int i = 0; i < k; i++) {
+				array[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			combination(0, 0);
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+	public static void combination(int cnt, int start) {
+		if (cnt == 6) {
+			for (int i = 0; i < 6; i++) {
+				sb.append(selected[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = start; i < k; i++) {
+			selected[cnt] = array[i];
+			combination(cnt + 1, i + 1);
+		}
+	}
+}
