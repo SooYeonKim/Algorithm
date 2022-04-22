04eb422fb0723df79d90823d7a213e616ac569b1
SooYeonKim <lovesu5632@gmail.com>
2022-04-22 21:38:50 +0900

    Add: 2022-04-22

---
+++ BOJ/Java/BOJ_1931.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class BOJ_1931 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] array = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			array[i][0] = Integer.parseInt(st.nextToken());
+			array[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(array, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[1] == o2[1]) {
+					return o1[0] - o2[0];
+				}
+				return o1[1] - o2[1];
+			}
+		});
+		
+		int result = 0;
+		int c = 0;
+		for (int i = 0; i < N; i++) {
+			if (array[i][0] < c) continue;
+			result++;
+			c = array[i][1];
+		}
+		
+		System.out.println(result);
+	}
+
+}
