4b67d883551e12d8ffbeff8cf09f36c01f4aaa10
SooYeonKim <lovesu5632@gmail.com>
2022-06-14 13:28:48 +0900

    Add: 2022-06-14

---
+++ Programmers/파괴되지_않은_건물.java
@@ -0,0 +1,30 @@
+
+public class 파괴되지_않은_건물 {
+    public int solution(int[][] board, int[][] skill) {
+        int[][] prefixSum = new int[board.length + 2][board[0].length + 2];
+        for (int i = 0; i < skill.length; i++) {
+            int type = skill[i][0];
+            int r1 = skill[i][1];
+            int c1 = skill[i][2];
+            int r2 = skill[i][3];
+            int c2 = skill[i][4];
+            int degree= skill[i][5];
+            
+            if (type == 1) degree *= -1;
+            
+            prefixSum[r1 + 1][c1 + 1] += degree;
+            prefixSum[r1 + 1][c2 + 2] -= degree;
+            prefixSum[r2 + 2][c1 + 1] -= degree;
+            prefixSum[r2 + 2][c2 + 2] += degree;
+        }
+        
+        int answer = 0;
+        for (int i = 1; i < board.length + 1; i++) {
+            for (int j = 1; j < board[0].length + 1; j++) {
+                prefixSum[i][j] += prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1];
+                if (prefixSum[i][j] + board[i-1][j-1] > 0) answer++;
+            }
+        }
+        return answer;
+    }
+}
