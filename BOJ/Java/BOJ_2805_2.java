2baaf1d12b3d52ddbaff23461bd9b41d046571e4
SooYeonKim <lovesu5632@gmail.com>
2022-10-31 21:04:17 +0900

    Add: 2022-10-31

---
+++ BOJ/Java/BOJ_2805_2.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2805_2 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] array = new int[N];
+		int max = 0;
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			array[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, array[i]);
+		}
+		
+		int left = 0;
+		int right = max;
+		int result = 0;
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			
+			long sum = 0;
+			for (int i = 0; i < N; i++) {
+				if (array[i] < mid) continue;
+				sum += (array[i] - mid);
+			}
+			
+			if (sum >= M) {
+				result = mid;
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		
+		System.out.println(result);
+	}
+
+}
