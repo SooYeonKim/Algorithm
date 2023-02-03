466f1e04ddf76c21b7d08d249b00930560585eb5
SooYeonKim <lovesu5632@gmail.com>
2023-02-03 23:08:57 +0900

    Add: 2023-02-03

---
+++ Programmers/점_찍기.java
@@ -0,0 +1,11 @@
+
+public class 점_찍기 {
+    public long solution(int k, int d) {
+        long answer = 0;
+        for (int i = 0; i <= d; i += k) {
+            int y = (int)Math.sqrt((long)d * d - (long)i * i);
+            answer += (y / k + 1);
+        }
+        return answer;
+    }
+}
